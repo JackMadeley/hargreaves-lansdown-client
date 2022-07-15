600d1109a12c3d7f51fa5ac73a6762f1c80ee46a
Jack Madeley <jm@jackmadeley.co.uk>
2022-07-15 09:02:43 +0100

    Updating test_hl_client.py

--- tests/test_hl_client.py
+++ tests/test_hl_client.py
@@ -10,6 +10,8 @@ class TestHLClient(unittest.TestCase):
 
     def setUp(self):
         self.client = HLClient()
+        self.client.login(username=os.getenv(key='hl_username'), date_of_birth=os.getenv(key='hl_dob'),
+                          password=os.getenv(key='hl_password'), secure_number=os.getenv(key='hl_secure_number'))
 
     def test_hl_client_login(self):
         if len(self.client.cookie_jar) == 0:
