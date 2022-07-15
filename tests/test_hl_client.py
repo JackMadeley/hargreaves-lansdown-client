f74d96b38539c5bca8fc45752bb431f5bb01778b
Jack Madeley <jm@jackmadeley.co.uk>
2022-07-15 08:45:26 +0100

    Adding if clause to test_login method

--- tests/test_hl_client.py
+++ tests/test_hl_client.py
@@ -12,8 +12,9 @@ class TestHLClient(unittest.TestCase):
         self.client = HLClient()
 
     def test_hl_client_login(self):
-        self.client.login(username=os.getenv(key='hl_username'), date_of_birth=os.getenv(key='hl_dob'),
-                          password=os.getenv(key='hl_password'), secure_number=os.getenv(key='hl_secure_number'))
+        if len(self.client.cookie_jar) == 0:
+            self.client.login(username=os.getenv(key='hl_username'), date_of_birth=os.getenv(key='hl_dob'),
+                              password=os.getenv(key='hl_password'), secure_number=os.getenv(key='hl_secure_number'))
         self.assertEqual(len(self.client.cookie_jar), 2)
 
     def test_get_account_links(self):
